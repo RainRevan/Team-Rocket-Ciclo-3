8b13016daa72f98efb1e05e630b9281792a68a26
Javier Alejandro Rincón <110916364+RainRevan@users.noreply.github.com>
2022-09-23 16:49:34 -0500

    Update Index.js
    add

--- Public/js/Index.js
+++ Public/js/Index.js
@@ -1,1 +1,15 @@
-const nombre = document.getElementByID
+const proyecto = document.getElementById("proyecto");
+const nombre = document.getElementById("nombre");
+const email = document.getElementById("email");
+const organizacion = document.getElementById("organizacion");
+const form = document.getElementById("form");
+//const listInputs = document.getquerySelectorAll("input");//
+//console.log(listInputs);//
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (nombre.value.length <1 ||nombre.value.trim() == "") {
+    console.error("error");
+  }
+  if (apellidos.value.length <1 ||apellidos.value.trim() == "") {
+    console.error("error");
+});
