2abff744fd199213694c080c9311a66a3d500455
Javier Alejandro Rincón <110916364+RainRevan@users.noreply.github.com>
2022-09-24 11:18:17 -0500

    Ajustes y mejoras html
    se mejora la estructura del index

--- Public/js/Index.js
+++ Public/js/Index.js
@@ -3,13 +3,13 @@ const nombre = document.getElementById("nombre");
 const email = document.getElementById("email");
 const organizacion = document.getElementById("organizacion");
 const form = document.getElementById("form");
-//const listInputs = document.getquerySelectorAll("input");//
-//console.log(listInputs);//
+const listInputs = document.getquerySelectorAll("input");
+console.log(listInputs);
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (nombre.value.length <1 ||nombre.value.trim() == "") {
+  if (nombre.value.length < 1 || nombre.value.trim() == "") {
     console.error("error");
   }
-  if (apellidos.value.length <1 ||apellidos.value.trim() == "") {
+  if (apellidos.value.length < 1 || apellidos.value.trim() == "") {
     console.error("error");
-});
+  });
