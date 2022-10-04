9ffb18e5e67e2055ece6d98f30b0543185a3a5ca
Javier Alejandro Rincón <110916364+RainRevan@users.noreply.github.com>
2022-10-03 20:53:56 -0500

    paquetes
    Se añaden paquetes

--- APIformulario/app.js
+++ APIformulario/app.js
@@ -1,18 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
 // Uso del paquete Pg
-const {Client} = require('pg');
-// conexio desde Heroku
-const connectionString = 'postgres://vgullytuuvytyz:8801e4f105c7e15e4b71c5fab86145f89e879f8e597aa661bd9b0f9e32d2353f@ec2-52-207-15-147.compute-1.amazonaws.com:5432/dboifi9jihrmdj'
-// Configuracion de conexion
-const connectionData = {
-    connectionString : connectionString,
-    ssl:{ rejectUnauthorized: false}
-};
+const { Client } = require('pg');
 
-const client = new Client(connectionData);
-client.connect();
-// Querys
-//Obtener información
-client.query('SELECT * FROM public."Usuario"')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hola mundo!");
+});
+
+app.get("/usuarios", (req, res) => {
+    // conexio desde Heroku
+    const connectionString = 'postgres://vgullytuuvytyz:8801e4f105c7e15e4b71c5fab86145f89e879f8e597aa661bd9b0f9e32d2353f@ec2-52-207-15-147.compute-1.amazonaws.com:5432/dboifi9jihrmdj'
+    // Configuracion de conexion
+    const connectionData = {
+        connectionString: connectionString,
+        ssl: { rejectUnauthorized: false }
+    };
+
+    const client = new Client(connectionData);
+    client.connect();
+    // Querys
+    //Obtener información
+    client.query('SELECT * FROM public."Usuario"')
         .then(response => {
             console.log(response.rows)
             client.end()
@@ -21,3 +36,9 @@ client.query('SELECT * FROM public."Usuario"')
             console.log(err);
             client.end()
         });
+
+});
+
+app.listen(port, () =>
+    console.log('Example app listening at http://localhost:${port}')
+);
